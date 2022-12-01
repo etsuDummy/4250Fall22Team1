540863eab9776c666441af67ab746a08253b1295
ShaunMBennett <89603464+ShaunMBennett@users.noreply.github.com>
2022-12-01 11:08:27 -0500

    Add files via upload

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using team3.Entities;
+
+namespace team3.Services;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    DbSet<Tasks> TaskList => Set<Tasks>();
+}
